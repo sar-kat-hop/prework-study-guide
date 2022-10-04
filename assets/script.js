3e0652c72e637601004f4b4bdd5945f27da4b103
Serenity <sarah.k.hopper@gmail.com>
2022-10-03 23:29:12 -0700

    add JavaScript (script.js) with loop to print topics to console, add strong header in style.css to better differentiate subheader added to index.html

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript', 'Vim'];
+
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+
+
+//Beginning of examples
+    
+/*
+var topic = "HTML";
+
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+    console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else if (topic === 'Vim') {
+    console.log("Let's learn some Vim shortcuts!")
+} else {
+    console.log('Please try again!');
+}
+*/
+
+/* EXAMPLE FOR LOOP
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+}
+*/
+
+//End of examples
+
+}
+
