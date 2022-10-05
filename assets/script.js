1a04caab2a74792a813db017742a59e778db93fa
Serenity <sarah.k.hopper@gmail.com>
2022-10-05 00:07:37 -0700

    add function to list all webpage topics (sections) in console log, add function to randomly choose topic and log in console, add explanatory messages/prompts to print to console before each function runs

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,35 @@
+//Prework study guide website JS
+
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript', 'Vim'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)]
 
-for(var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
-
-
-//Beginning of examples
-    
-/*
-var topic = "HTML";
-
-if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-    console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-    console.log("Let's study JavaScript!");
-} else if (topic === 'Vim') {
-    console.log("Let's learn some Vim shortcuts!")
-} else {
-    console.log('Please try again!');
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
-*/
+
+function selectTopic() {
+    if (randomTopic === 'HTML') {
+        console.log("How about HTML?");
+    } else if (randomTopic === 'CSS') {
+        console.log("Let's read about CSS.");
+    } else if (randomTopic === 'Git') {
+        console.log("How about Git?");
+    } else if (randomTopic === 'JavaScript') {
+        console.log("Let's take a look at JavaScript.");
+    } else if (randomTopic === 'Vim') {
+        console.log("Let's memorize some important Vim shortcuts.")
+    } else {
+        console.log("It looks like that didn't work as expected. Give it another try.");
+    }
+}
+
+console.log("These are the topics we learned about through the coding bootcamp's prework assignments, with some Vim shortcuts thrown in as an extra bonus.")
+listTopics();
+
+console.log("Which topic should we study first?")
+selectTopic();
 
 /* EXAMPLE FOR LOOP
 var shapes = ["triangle", "square", "pentagon", "circle"];
@@ -31,7 +38,5 @@ for(var x = 0; x < shapes.length; x++) {
 }
 */
 
-//End of examples
 
-}
 
